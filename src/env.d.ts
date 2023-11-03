871a1105352976de52930246a10d16f7c116400c
Blake Regalia <blake.regalia@gmail.com>
2023-11-03 00:28:51 -0700

    feat: bundle binary

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+
+declare module '*.wasm' {
+	const sb64_contents: string;
+	export default sb64_contents;
+}
+
+declare module '*.wasm?gzip' {
+	const sb64_data: string;
+	export default sb64_data;
+}
